8175ec95992e723de45fd906adbf3712db619da5
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-03-25 23:30:41 +0100

    first commit

---
+++ src/test/java/com/emat/jspring_mongo/JspringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emat.jspring_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
