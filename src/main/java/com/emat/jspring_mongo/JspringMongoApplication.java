76bd33fbba63f46fb2904389b71f240b69d73bf6
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-04-02 18:32:30 +0200

    configuration and spring app file

--- src/main/java/com/emat/jspring_mongo/JspringMongoApplication.java
+++ src/main/java/com/emat/jspring_mongo/JspringMongoApplication.java
@@ -2,8 +2,10 @@ package com.emat.jspring_mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(value = {"com.emat.jspring_mongo.repository"})
 public class JspringMongoApplication {
 
     public static void main(String[] args) {
