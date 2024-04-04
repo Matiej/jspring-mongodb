0f465b7d293200ef174fd86eaae8db2728d5a4e7
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-04-04 23:31:59 +0200

    basic string repository methods

--- src/main/java/com/emat/jspring_mongo/student/database/StudentRepository.java
+++ src/main/java/com/emat/jspring_mongo/student/database/StudentRepository.java
@@ -4,6 +4,11 @@ import com.emat.jspring_mongo.student.entity.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
+    List<Student> findByName(String name);
+    Optional<Student> findByNameAndMail(String name, String mail);
 }
